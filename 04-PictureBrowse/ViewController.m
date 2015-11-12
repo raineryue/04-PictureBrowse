194c3804f850c54e0a67cbd12887f9acdf8b6e41
raineryue <jinlei_yue@sina.com>
2015-11-12 16:57:45 +0800

    使用plist作为数据源，实现签单的照片器功能

--- 04-PictureBrowse/ViewController.m
+++ 04-PictureBrowse/ViewController.m
@@ -22,8 +22,14 @@
 /** 下一张图片按钮 */
 @property (nonatomic, weak) UIButton *nextButton;
 
-/** 图片内容描述*/
+/** 图片内容描述 */
 @property (nonatomic, weak) UILabel *descriptionLabel;
+
+/** 图片数据 */
+@property (nonatomic, strong) NSArray *imageArray;
+
+/** 当前图片的索引 */
+@property (nonatomic, assign) int currenIndex;
 
 @end
 
@@ -34,62 +40,140 @@
 
     // 1.添加子控件到控制器的视图上
     [self setupSubviews];
+    
+    // 2.初始化当前图片的索引
+    self.currenIndex = -1;
+    
+    // 3.模拟第一次点击
+    [self nextButtonClickAction];
 }
 
+/**
+ *  添加子控件到控制器的视图上
+ */
 - (void)setupSubviews {
-    // 1.当前浏览图片的数量
-    UILabel *countLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 40, self.view.bounds.size.width, 40)];
-    
-    countLabel.text = @"1/5";
-    countLabel.textAlignment = NSTextAlignmentCenter;
-    countLabel.font = [UIFont systemFontOfSize:18.0];
-    
-    self.countLabel = countLabel;
-    [self.view addSubview:self.countLabel];
-    
-    // 2.当前浏览到的图片
-    UIImageView *pictureImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"biaoqingdi"]];
-
-    CGFloat pictureImageViewW = 250;
-    CGFloat pictureImageViewH = 250;
-    CGFloat pictureImageViewX = (self.view.bounds.size.width - pictureImageViewW) * 0.5;
-    CGFloat pictureImageViewY = CGRectGetMaxY(self.countLabel.frame) + 20;
-    
-    pictureImageView.frame = CGRectMake(pictureImageViewX, pictureImageViewY, pictureImageViewW, pictureImageViewH);
-    
-    self.pictureImageView = pictureImageView;
-    [self.view addSubview:self.pictureImageView];
-    
-    // 3.上一张图片按钮
+    // 1.上一张图片按钮
     UIButton *prevButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
     prevButton.center = CGPointMake(self.pictureImageView.frame.origin.x * 0.5, self.pictureImageView.center.y);
     
     [prevButton setBackgroundImage:[UIImage imageNamed:@"left_normal"] forState:UIControlStateNormal];
     [prevButton setBackgroundImage:[UIImage imageNamed:@"left_highlighted"] forState:UIControlStateHighlighted];
+    [prevButton addTarget:self action:@selector(prevButtonClickAction) forControlEvents:UIControlEventTouchUpInside];
     
     self.prevButton = prevButton;
     [self.view addSubview:self.prevButton];
     
-    // 4.下一张图片按钮
+    // 2.下一张图片按钮
     UIButton *nextButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
     nextButton.center = CGPointMake(self.view.bounds.size.width - self.pictureImageView.frame.origin.x * 0.5, self.pictureImageView.center.y);
     
     [nextButton setBackgroundImage:[UIImage imageNamed:@"right_normal"] forState:UIControlStateNormal];
     [nextButton setBackgroundImage:[UIImage imageNamed:@"right_highlighted"] forState:UIControlStateHighlighted];
+    [nextButton addTarget:self action:@selector(nextButtonClickAction) forControlEvents:UIControlEventTouchUpInside];
     
     self.nextButton = nextButton;
     [self.view addSubview:self.nextButton];
+}
+
+#pragma mark - 按钮点击事件处理
+/**
+ *  上一张按钮点击事件
+ */
+- (void)prevButtonClickAction{
+    self.currenIndex --;
     
-    // 5.当前浏览图片内容的描述
-    UILabel *descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.pictureImageView.frame), self.view.bounds.size.width, 180)];
+    [self changeDataForView];
+}
+
+/**
+ *  下一张按钮点击事件
+ */
+- (void)nextButtonClickAction{
+    self.currenIndex ++;
     
-    descriptionLabel.text = @"在他面前，神马表情都弱爆了，有木有！！！在他面前，神马表情都弱爆了，有木有！！！在他面前，神马表情都弱爆了，有木有！！！重要的事情要说三遍";
-    descriptionLabel.textAlignment = NSTextAlignmentCenter;
-    descriptionLabel.numberOfLines = 0;
-    descriptionLabel.font = [UIFont systemFontOfSize:18.0];
+    [self changeDataForView];
+}
+
+#pragma mark - 私有辅助方法
+/**
+ *  切换视图数据内容
+ */
+- (void)changeDataForView {
+    self.prevButton.enabled = self.currenIndex > 0;
+    self.nextButton.enabled = self.currenIndex < self.imageArray.count - 1;
     
-    self.descriptionLabel = descriptionLabel;
-    [self.view addSubview:self.descriptionLabel];
+    NSDictionary *dictionary = [self.imageArray objectAtIndex:self.currenIndex];
+    
+    self.countLabel.text = [NSString stringWithFormat:@"%d/%d", self.currenIndex + 1, (int)self.imageArray.count];
+    self.pictureImageView.image = [UIImage imageNamed:dictionary[@"name"]];
+    self.descriptionLabel.text = dictionary[@"desc"];
+}
+
+#pragma mark - 属性懒加载
+/**
+ *  当前浏览图片的数量
+ */
+- (UILabel *)countLabel {
+    if (nil == _countLabel) {
+        UILabel *countLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 40, self.view.bounds.size.width, 40)];
+        countLabel.textAlignment = NSTextAlignmentCenter;
+        countLabel.font = [UIFont systemFontOfSize:18.0];
+        
+        _countLabel = countLabel;
+        [self.view addSubview:_countLabel];
+    }
+    
+    return _countLabel;
+}
+
+/**
+ *  当前图片
+ */
+- (UIImageView *)pictureImageView {
+    if (nil == _pictureImageView) {
+        CGFloat pictureImageViewW = 200;
+        CGFloat pictureImageViewH = 200;
+        CGFloat pictureImageViewX = (self.view.bounds.size.width - pictureImageViewW) * 0.5;
+        CGFloat pictureImageViewY = CGRectGetMaxY(self.countLabel.frame) + 20;
+        
+        UIImageView *pictureImageView = [[UIImageView alloc] initWithFrame:CGRectMake(pictureImageViewX, pictureImageViewY, pictureImageViewW, pictureImageViewH)];
+        
+        _pictureImageView = pictureImageView;
+        [self.view addSubview:_pictureImageView];
+    }
+    
+    return _pictureImageView;
+}
+
+/** 
+ *  图片内容描述 
+ */
+- (UILabel *)descriptionLabel {
+    if (nil == _descriptionLabel) {
+        UILabel *descriptionLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, CGRectGetMaxY(self.pictureImageView.frame), self.view.bounds.size.width, 180)];
+        
+        descriptionLabel.textAlignment = NSTextAlignmentCenter;
+        descriptionLabel.numberOfLines = 0;
+        descriptionLabel.font = [UIFont systemFontOfSize:18.0];
+        
+        _descriptionLabel = descriptionLabel;
+        [self.view addSubview:_descriptionLabel];
+    }
+    
+    return _descriptionLabel;
+}
+
+/**
+ *  懒加载
+ */
+- (NSArray *)imageArray {
+    if (nil == _imageArray) {
+        NSString *imagePath = [[NSBundle mainBundle] pathForResource:@"ImageData" ofType:@"plist"];
+        
+        _imageArray = [NSArray arrayWithContentsOfFile:imagePath];
+    }
+    
+    return _imageArray;
 }
 
 - (void)didReceiveMemoryWarning {
